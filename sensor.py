a0b85602a61f783bcad3e18053f0ef44004461a7
EricSchrock <eric.schrock22@gmail.com>
2023-10-15 12:08:28 -0900

    Basic I2C connection to CO2 sensor

--- sensor.py
+++ sensor.py
@@ -1,10 +1,16 @@
 # Save this file to the Pico as "main.py" to have it run on startup
 
-from machine import Pin
+from machine import I2C, Pin
 from time import sleep
 
 led = Pin("LED", Pin.OUT)
+i2c = I2C(0, scl=Pin("GP17"), sda=Pin("GP16"))
 
 while True:
     led.toggle()
-    sleep(1)
+    value = 0x2
+    i2c.writeto_mem(0x53, 0x10, value.to_bytes(1, 'little'))
+    mode = int.from_bytes(i2c.readfrom_mem(0x53, 0x10, 1), 'little')
+    co2 = int.from_bytes(i2c.readfrom_mem(0x53, 0x24, 2), 'little')
+    print(f"co2: {co2}")
+    sleep(0.1)
