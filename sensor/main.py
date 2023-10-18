f41985a65877f302135c111233b38b1cb68f4710
Eric Schrock <eric.schrock22@gmail.com>
2023-10-17 20:54:25 -0400

    Limit sensor/main.py import scope

--- sensor/main.py
+++ sensor/main.py
@@ -1,36 +1,36 @@
-from machine import ADC, Pin, unique_id
-import network
-import socket
-import time
+from machine import Pin, unique_id
+from network import STA_IF, STAT_CONNECTING, WLAN
+from socket import socket
+from time import sleep, sleep_ms
 
 from config import SSID, PWD, IP, PORT
 from ens160 import ENS160
 
-LED = Pin("LED", Pin.OUT)
+led = Pin("LED", Pin.OUT)
 
-wlan = network.WLAN(network.STA_IF)
+wlan = WLAN(STA_IF)
 wlan.active(True)
 wlan.connect(SSID, PWD)
-while wlan.status() == network.STAT_CONNECTING:
-    LED.toggle()
-    time.sleep_ms(250)
+while wlan.status() == STAT_CONNECTING:
+    led.toggle()
+    sleep_ms(250)
 if wlan.isconnected():
-    LED.on()
+    led.on()
 
 sensor = ENS160()         # CO2 Sensor attached via I2C
 sensor.opmode(0x02)       # Enable sensor readings
 uid = unique_id().hex()   # Unique device ID
 while True:
     try:
-        client = socket.socket()
+        client = socket()
         client.connect((IP, PORT))
         client.sendall(uid.encode())
         while True:
-            LED.on()
+            led.on()
             client.sendall(str(sensor.eco2()).encode())
-            LED.off()
-            time.sleep(30)
+            led.off()
+            sleep(30)
     except OSError:
         client.close()
-        LED.on()
-        time.sleep(60)
+        led.on()
+        sleep(60)
